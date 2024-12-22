2d971889d9751e8b805de5dcb94a822728b11a99
Hell1on <artem.bondarchuk1233@gmail.com>
2024-12-22 19:24:41 +0200

    Added parsing logic

--- Tasks/Task4.js
+++ Tasks/Task4.js
@@ -5,11 +5,25 @@ const readline = require('readline');
 
 function sumColumn(filePath, columnName) {
   return new Promise((resolve, reject) => {
+    let total = 0;
+    let header = [];
+
     const stream = fs.createReadStream(filePath);
 
     const rl = readline.createInterface({
       input: stream,
       crlfDelay: Infinity,
+    });
+
+    rl.on('line', (line) => {
+      const columns = line.split('\t');
+      if (header.length === 0) {
+        header = columns;
+      } else {
+        const columnIndex = header.indexOf(columnName);
+        const value = parseFloat(columns[columnIndex]);
+        total += value;
+      }
     });
   });
 }
