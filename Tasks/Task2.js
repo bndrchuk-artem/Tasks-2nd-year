830dd32f1c59a0a9ead85a130c404b2309f72040
Hell1on <artem.bondarchuk1233@gmail.com>
2024-12-22 14:39:12 +0200

    added base structure

--- Tasks/Task2.js
+++ Tasks/Task2.js
@@ -1,38 +1,21 @@
 'use strict';
 
-const asyncFilter = (array, asyncCallback, resultCallback) => {
-  const arrayLength = array.length;
-  const arrayOfResults = new Array(arrayLength).fill(null);
-  let counter = 0;
+const asyncFilter = (array, asyncChecker) => {
 
-  array.forEach((item, index) => {
-    asyncCallback(item, (err, include) => {
-      if (err) {
-        return resultCallback(err, null);
-      }
-
-      if (include) {
-        arrayOfResults[index] = item;
-      }
-
-      if (++counter === arrayLength) {
-        resultCallback(null, arrayOfResults.filter((item) => item !== null));
-      }
-    });
-  });
 };
 
 // Use case
 let i = 0;
 asyncFilter(
   [1, 2, 3, 4, 5, 6],
-  (number, cb) => {
-    setTimeout(() => {
-      cb(null, number % 2 === 0);
-    }, 1000 - i * 100);
-    i++;
-  },
-  (err, result) => {
-    console.log(err, result); // null [2, 4, 6]
-  }
-);
+  (number) =>
+    new Promise((resolve, reject) => {
+      setTimeout();
+    })
+)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
