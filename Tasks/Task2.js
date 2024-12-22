db3a5599894520197b3b5066fc86755476787787
Hell1on <artem.bondarchuk1233@gmail.com>
2024-12-22 14:47:53 +0200

    added async filter

--- Tasks/Task2.js
+++ Tasks/Task2.js
@@ -1,16 +1,26 @@
 'use strict';
 
 const asyncFilter = (array, asyncChecker) => {
-
+  const promises = array.map((item) => {
+    asyncChecker(item)
+      .then((include) => {
+        if (!include) {
+          return null;
+        }
+        return item;
+      });
+  });
+  return Promise.all(promises)
+    .then((results) => results.filter((item) => item !== null));
 };
 
 // Use case
 let i = 0;
 asyncFilter(
   [1, 2, 3, 4, 5, 6],
-  (number) =>
+  (item) =>
     new Promise((resolve, reject) => {
-      setTimeout();
+      setTimeout(() => resolve(item % 2 === 0), 1000);
     })
 )
   .then((result) => {
