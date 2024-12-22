8282628c4e8086ccd10e5006e729332cd5c5ece7
Hell1on <artem.bondarchuk1233@gmail.com>
2024-12-22 16:51:23 +0200

    Added cleanup for event listeners

--- Tasks/Task3.js
+++ Tasks/Task3.js
@@ -1,24 +1,26 @@
 'use strict';
 
 const asyncFilter = (array, asyncChecker, signal) => {
+  const abortError = new Error('Operation aborted');
   const promises = array.map((item) =>
     new Promise((resolve, reject) => {
-      if (signal.aborted) {
-        reject('Aborted');
-        return;
-      }
-      signal.addEventListener('abort', () => {
-        reject('Aborted');
-      });
+      if (signal.aborted) reject(abortError);
+
+      const listener = () => reject(abortError);
+      signal.addEventListener('abort', listener);
+
       asyncChecker(item)
         .then((result) => {
+          signal.removeEventListener('abort', listener);
           resolve(result ? item : null);
         })
         .catch((err) => {
+          signal.removeEventListener('abort', listener);
           reject(err);
         });
     })
   );
+
   return Promise.all(promises).then((results) =>
     results.filter((item) => item !== null)
   );
@@ -32,7 +34,7 @@ asyncFilter(
   [1, 2, 3, 4, 5, 6],
   (item) =>
     new Promise((resolve) => {
-      setTimeout(() => resolve(item % 2 === 0), 1600);
+      setTimeout(() => resolve(item % 2 === 0), 1200);
     }),
   signal
 )
