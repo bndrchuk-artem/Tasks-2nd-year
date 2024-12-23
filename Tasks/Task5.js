7f165f8cdc141fa57aaf8dbb2e39a4f28a412e39
Hell1on <artem.bondarchuk1233@gmail.com>
2024-12-23 15:03:48 +0200

    Added DataProducer class to generate and emit data

--- Tasks/Task5.js
+++ Tasks/Task5.js
@@ -2,3 +2,18 @@
 
 const EventEmitter = require('events');
 const ee = new EventEmitter();
+
+class DataProducer {
+  constructor() {
+    this.counter = 0;
+  }
+
+  async produceData() {
+    setInterval(() => {
+      const data = { id: ++this.counter, value: Math.random() * 100 };
+      console.log('Generated data:', data);
+      ee.emit('newData', data);
+    }, 1000); // Generates and sends data every 1000 ms
+  }
+}
+
