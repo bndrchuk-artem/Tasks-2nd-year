568ff435531e3b920594290ae4d08c4bfba08756
Hell1on <artem.bondarchuk1233@gmail.com>
2024-12-29 18:46:09 +0200

    added error checking

--- Tasks/Task1.js
+++ Tasks/Task1.js
@@ -4,15 +4,19 @@ const asyncFilter = (array, asyncCallback, resultCallback) => {
   const arrayLength = array.length;
   const arrayOfResults = new Array(arrayLength).fill(null);
   let counter = 0;
+  let errorExist = false;
 
   array.forEach((item, index) => {
     asyncCallback(item, (err, include) => {
+      if (errorExist) return;
       if (err) {
+        errorExist = true;
         return resultCallback(err, null);
       }
 
       if (include) {
         arrayOfResults[index] = item;
+        console.log(item);
       }
 
       if (++counter === arrayLength) {
@@ -25,11 +29,12 @@ const asyncFilter = (array, asyncCallback, resultCallback) => {
 // Use case
 let i = 0;
 asyncFilter(
-  [1, 2, 3, 4, 5, 6],
+  [2, 2, 3, 4, 5, 6],
   (number, cb) => {
     setTimeout(() => {
-      cb(null, number % 2 === 0);
-    }, 1000 - i * 100);
+      if (number <= 3) cb(new Error('Some error'), null);
+      else cb(null, number % 2 === 0);
+    }, 1000 - 100 * i);
     i++;
   },
   (err, result) => {
